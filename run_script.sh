ea52b3f56dcdaf65ecb77f92c8af6ca64c247956
Rohith Bojja <119781796+rohithbojja@users.noreply.github.com>
2024-07-18 23:25:51 +0530

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -4,8 +4,58 @@
 echo "Current directory: $(pwd)"
 echo "List of files in the current directory: $(ls)"
 
+# Default values
+host="0.0.0.0"
+port="11436"
+model_path="/workspace/llama.cpp/unsloth.Q4_K_M.gguf"
+threads="12"
+cache_type_k="f16"
+chat_template="chatml"
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -h|--host)
+        host="$2"
+        shift
+        shift
+        ;;
+        -p|--port)
+        port="$2"
+        shift
+        shift
+        ;;
+        -m|--model)
+        model_path="$2"
+        shift
+        shift
+        ;;
+        -t|--threads)
+        threads="$2"
+        shift
+        shift
+        ;;
+        -ctk|--cache-type-k)
+        cache_type_k="$2"
+        shift
+        shift
+        ;;
+        --chat-template)
+        chat_template="$2"
+        shift
+        shift
+        ;;
+        *)
+        # unknown option
+        shift
+        ;;
+    esac
+done
+
 # Define the command
-cmd="/llama-server -m /workspace/llama.cpp/unsloth.Q4_K_M.gguf --host 0.0.0.0 --port 11436 --chat-template chatml -t 12 -ngl 0 -ctk f16 "
+cmd="/llama-server -m \"$model_path\" --host \"$host\" --port \"$port\" --chat-template \"$chat_template\" -t \"$threads\" -ctk \"$cache_type_k\""
 
 # Start the process in the background
 $cmd &
